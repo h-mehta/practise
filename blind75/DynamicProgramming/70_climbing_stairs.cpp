af3d19182d38b1f54cedee8f22402865ab2f3750
Harshit Mehta <hm2540@nyu.edu>
2024-02-11 19:26:02 -0500

    Initial DP commit

---
+++ blind75/DynamicProgramming/70_climbing_stairs.cpp
@@ -0,0 +1,58 @@
+
+// Link: https://leetcode.com/problems/climbing-stairs/description/
+/* Description:
+
+70. Climbing Stairs
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+
+*/
+
+
+class Solution {
+public:
+    unordered_map<int, int> dp;
+    int climbStairs(int n) {
+        if(n==0){
+            return 1;
+        } else if(n<0){
+            return 0;
+        }
+        if(dp.contains(n)){
+            return dp[n];
+        }
+
+        int left = climbStairs(n-1);
+        int right = climbStairs(n-2);
+
+        dp[n] = left+right;
+        return dp[n];
+    }
+};
+
+
